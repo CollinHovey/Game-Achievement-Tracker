80a175f5507b7b8438e1fda222504ce56e2e7a63
Collin Hovey <collinhovey@gmail.com>
2022-08-17 19:34:46 +0000

    Made Logged Out Page

--- client/components/home-logged-out.jsx
+++ client/components/home-logged-out.jsx
@@ -4,8 +4,25 @@ import UserContext from '../lib/user-context';
 export default class HomeLoggedOut extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { navOpen: 'initial' };
+    this.state = {
+      shadowOn: false,
+      posts: null
+    };
     this.login = this.login.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/posts', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json()
+        .then(data => {
+          this.setState({ posts: data });
+        })
+      );
   }
 
   login() {
@@ -13,11 +30,37 @@ export default class HomeLoggedOut extends React.Component {
   }
 
   render() {
+    let postsList = <h1>Loading Posts</h1>;
+    const posts = this.state.posts;
+    if (this.state.posts !== null) {
+      postsList = posts.map((post, index) => {
+        return (
+            <div key={index} className='post-container'>
+              <div className='post-header'>
+                <h1 className='post-topic'>{post.topic}</h1>
+                <h1 className='post-username'>{post.username}</h1>
+              </div>
+              <div className='caption-container'>
+                <hr className='caption-line'></hr>
+                <p className='post-caption'>{post.caption}</p>
+              </div>
+              <hr className='post-line'></hr>
+            </div>
+        );
+      });
+    }
     return (
-      <>
-      </>
+        <>
+          <div className='post-list'>
+            <div className='home-logged-in-header-container'>
+              <input className='search-input' placeholder='Search'></input>
+            </div>
+            {postsList}
+          </div>
+        </>
     );
   }
+
 }
 
 HomeLoggedOut.contextType = UserContext;
